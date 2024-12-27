caa0d30c1ee2ede58f74ef811fb186830aac159e
saurabh eknath dokade <saurabhdokade77@gmail.com>
2024-12-27 16:00:29 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -49,7 +49,7 @@ export default defineConfig(({ mode }) => {
     ],
     resolve: {
       alias: {
-        "@": path.resolve("./src"),
+        '@': path.resolve(__dirname, 'src'),
       },
     },
     optimizeDeps: {
